2869f9f212be4f1367d1cae1562c995528589d64
adkadi <adkateki@gmail.com>
2021-01-12 22:10:00 +0000

    fix guile

--- server/game/challenge.js
+++ server/game/challenge.js
@@ -98,11 +98,11 @@ class Challenge {
     }
 
     isAttacking(card) {
-        return this.isInitiated && this.attackers.includes(card);
+        return this.attackers.includes(card);
     }
 
     isDefending(card) {
-        return this.isInitiated && this.defenders.includes(card);
+        return this.defenders.includes(card);
     }
 
     isParticipating(card) {
