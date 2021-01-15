7100b6e59913270ff6f282c942e12c5fc5ebb47a
adkadi <adkateki@gmail.com>
2021-01-15 17:19:18 +0000

    fix myhsa achievement

--- server/game/achievements/01-SoTFM/TheyAlwaysGaveGoodCounsel.js
+++ server/game/achievements/01-SoTFM/TheyAlwaysGaveGoodCounsel.js
@@ -2,7 +2,7 @@ const Achievement = require('../../achievement.js');
 
 class TheyAlwaysGaveGoodCounsel extends Achievement {
      check(){
-        return this.owner.faction.name=="House Targaryen" && this.owner.game.allCards.filter(card => card.owner === this.owner && (card.name==='Missandei' || card.name==='Jorah Mormont')).length==0;
+        return this.owner.faction.name=="House Targaryen" && this.owner.game.allCards.filter(card => card.owner === this.owner && (card.name==='Missandei' || card.name==='Ser Jorah Mormont')).length==0;
      }
 }
 
